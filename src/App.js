0e818c3706acf106d9df207524475a5e3108bdc9
Johan Rimkahan <115785092+rimkahan888@users.noreply.github.com>
2025-03-17 17:44:41 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar';
 import News from './components/News';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
-   
+      
 const App = ()=> {
   const pageSize = 5;
   const apiKey = process.env.REACT_APP_NEWS_API
